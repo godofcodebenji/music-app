a16e26c72eded0cb7cb04fd85fd9ebafea5c1f60
Ben Stander <ben@limabean.co.za>
2017-01-23 10:45:55 +0200

    files for music app

---
+++ compiled/mxplayer.js
@@ -0,0 +1,1 @@
+var app=angular.module("mxPlayer",[]);app.controller("mxPlayer",function(a){a.title="Mx Music Player"});
---
+++ controllers/mxplayer.js
@@ -0,0 +1,5 @@
+var app = angular.module('mxPlayer', []);
+
+app.controller('mxPlayer',function($scope){
+	$scope.title = 'Mx Music Player';
+});
