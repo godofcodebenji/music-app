5566c950b0d608fc9831a3527957a85ef04959b7
Peter2962 <peter@limabean.co.za>
2017-01-31 17:19:56 +0200

    Added routes and created sidebar

--- controllers/mxplayer.js
+++ controllers/mxplayer.js
@@ -1,5 +1,14 @@
-var app = angular.module('mxPlayer', []);
+var app = angular.module('mxPlayer', ['ngRoute']);
 
-app.controller('mxPlayer',function($scope){
+app.controller('mxPlayer',function($scope, $route){
 	$scope.title = 'Mx Music Player';
+}).config(function($routeProvider){
+
+	$routeProvider
+	.when("/",{
+		templateUrl: "views/fragments/default.html"
+	}).when("/login", {
+		templateUrl: "views/templates/login/login.html"
+	});
+
 });
